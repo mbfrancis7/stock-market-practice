b87a6011f5b8ae36e03e79ac701630615877467e
Matthew Francis <mbfrancis7@gmail.com>
2019-01-19 19:05:47 -0500

    start from scratch

--- client/src/content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Content = props => {
-  return(
-    <h1 className="bodyContent">{props.content}</h1>
-  )
-}
-
-export default Content;
--- server/connection-string.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  authString: 'postgres://dreztgjq:d493Qxo9CTYcekEq24oiCDum22PS4kyF@baasu.db.elephantsql.com:5432/dreztgjq',
-  postString: 'postgres://cylyktcc:StrjELZOYecfNQ-6dEAS20EC-ixbb06-@baasu.db.elephantsql.com:5432/cylyktcc'
-}
