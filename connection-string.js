625c6be85fb802d72237c4e1536586af251dd341
mbfrancis7 <mbfrancis7@gmail.com>
2019-01-23 11:52:32 -0500

    Delete connection-string.js

--- connection-string.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  authString: 'postgres://inapjczt:dM-zR_JiSfachG8-YDUZKB3bc-LkqQ7j@baasu.db.elephantsql.com:5432/inapjczt',
-  postString: 'postgres://cylyktcc:StrjELZOYecfNQ-6dEAS20EC-ixbb06-@baasu.db.elephantsql.com:5432/cylyktcc'
-}
